37cc712aea758b4b0692f8425b511e265bf6d82e
teamlead11 <automationlead11@gmail.com>
2018-09-25 10:03:04 +0530

    added test case1

---
+++ src/testpack/TestCase1.java
@@ -0,0 +1,10 @@
+package testpack;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my testc ase1");
+	}
+
+}
