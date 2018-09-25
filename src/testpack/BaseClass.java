c031b4b24be502e1ccbf6d559395b9ff3cfccef2
teamlead11 <automationlead11@gmail.com>
2018-09-25 09:47:48 +0530

    modifed base class

--- src/testpack/BaseClass.java
+++ src/testpack/BaseClass.java
@@ -4,7 +4,9 @@ public class BaseClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("this is my base class");
+
+		System.out.println("this is my second base ");
+		System.out.println("this is my third base ");
 	}
 
 }
